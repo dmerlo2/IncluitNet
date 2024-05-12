2a6bf05a5212c1ce8ebad512d083b8a570d5fca0
dmerlo2 <dmerlosti@gmail.com>
2024-05-12 13:41:11 -0300

    Subida Clase2

---
+++ Clase2/TiposDatos/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//Datos numericos
+
+byte numeroByte = 12;
+int numeroInteger = 0;
+long numeroLong = 200;
+double numeroDouble = 300.123;
+float numeroFloat = 400;
+
+//Datos logicos
+bool datoBool = false; // 0
+bool datosBool2 = true; // 1
+
+//Datos de texto
+string datoString = "Texto";
+
+Console.WriteLine(datoString);
+Console.WriteLine("dato 2");
+
+Console.WriteLine("Ingrese valor: ");
+datoString = Console.ReadLine();
+
+Console.WriteLine(datoString);
+
+int datoInt = Convert.ToInt32(datoString);
+
+Console.WriteLine(datoInt);
---
+++ Clase2/TiposDatosVSCode/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//Datos numericos
+byte numeroByte = 0;
+int numeroInteger = 1234;
+decimal numeroDecimal = 300;
+float numeroFloat = 400;
+
+//Datos logico
+bool datoBool = false;
+
+//Datos texto
+string datosString = "texto de prueba numero uno";
+
+Console.WriteLine(datosString);
+
+Console.WriteLine(numeroByte);
+
