1f75ff796d1b990c8e13d76ce7582564734ba20a
Dmerlo <dmerlosti@hotmail.com>
2024-05-21 11:38:13 -0300

    Clase 4 Primer Subida

---
+++ Clase4/CONDICIONALES/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Ingrese se Edad: ");
+int edad = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese su equipo e futbol: ");
+string equipo = Console.ReadLine();
+
+bool condicion = edad < 18 && equipo == "RIVER";
+
+if (condicion)
+{
+    Console.WriteLine("Usted es menor ed ead");
+}
+else
+{
+    Console.WriteLine("Usted es mayor de edad");
+}
