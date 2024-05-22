97953f59eb6dc7c13bb78c8ec35504c11f98a46f
Dmerlo <dmerlosti@hotmail.com>
2024-05-22 09:01:55 -0300

    Clase 6 Primera Subida

---
+++ Clase6/Clase6/Program.cs
@@ -0,0 +1,28 @@
+﻿int numeroSecreto = new Random(DateTime.Now.Millisecond).Next(1, 21);
+
+int nVueltas = 1;
+
+Console.WriteLine("Ingrese numero: ");
+int numeroUsuario = int.Parse(Console.ReadLine());
+
+while (numeroSecreto != numeroUsuario)
+{
+    nVueltas++;
+    if (numeroUsuario > numeroSecreto)
+    {
+        Console.WriteLine($"Numero ingresado {numeroUsuario} es mayor a numero secreto");
+        Console.WriteLine("Ingrese nuevamente un numero");
+        numeroUsuario=int.Parse(Console.ReadLine());        
+    }
+    if (numeroUsuario < numeroSecreto)
+    {
+        Console.WriteLine($"Numero ingresado {numeroUsuario} es menor a numero secreto");
+        Console.WriteLine("Ingrese nuevamente un numero");
+        numeroUsuario = int.Parse(Console.ReadLine());
+    }
+    if (numeroUsuario == numeroSecreto){
+        Console.WriteLine($"Felicitaciones, has adivinado el numero secreto que era: {numeroSecreto} ");
+        Console.WriteLine($"Lo has logrado en {nVueltas} vueltas");
+    }
+
+}
