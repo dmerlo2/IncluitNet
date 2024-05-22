12b4883e4c7ec127e909bc190287293a9e8f42f1
dmerlo2 <dmerlosti@gmail.com>
2024-05-22 19:08:02 -0300

    Clase8 Ejercicio Vectores notas

---
+++ Clase8Matrices2/Clase8_EjercicioExamen/Program.cs
@@ -0,0 +1,23 @@
+﻿//Ejercicio vectores Clase8
+//Ingresar cantidad de alumnos
+//Ingresar por teclado nota de cada uno
+//Mostrar cada una de las notas
+
+Console.WriteLine("Ingrese cantidad de alumnos: ");
+int nCantidadAlumnos =  int.Parse(Console.ReadLine());
+
+int[] notas = new int[nCantidadAlumnos];
+
+for (int i = 0; i < nCantidadAlumnos; i++)
+{
+    Console.WriteLine($"Ingrese nota de alumno {i + 1}");
+    notas[i]= int.Parse(Console.ReadLine());
+}
+
+//Muestra notas ingresadas
+Console.WriteLine("Notas Ingresadas");
+Console.WriteLine("----- ----------");
+for(int i = 0;i < nCantidadAlumnos;i++)
+{
+    Console.WriteLine($"Nota del alumno {i + 1} es {notas[i]}");
+}
