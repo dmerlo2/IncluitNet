0c42116b696881638c3dcdf4963a3ac6c3775860
Dmerlo <dmerlosti@hotmail.com>
2024-05-24 13:45:14 -0300

    Clase9

---
+++ Clase10/Clase10/Program.cs
@@ -0,0 +1,10 @@
+﻿var personas = new List<string>();
+
+personas.Add("Ezequiel");
+personas.Add("Joaquin");
+personas.Add("Eugenio");
+
+foreach(var persona in personas)
+{
+    Console.WriteLine(persona);
+}
